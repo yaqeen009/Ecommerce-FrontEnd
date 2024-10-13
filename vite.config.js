971537310f4139f9a6d80b1463e931a3c1ad2622
yaqeen009 <wunzaligu19@gmail.com>
2024-10-13 13:05:45 +0000

    Tests setup

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    css: true,
-    setupFiles: './src/tests/setupTests.js', 
-  },
 })
